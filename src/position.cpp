465d6a75c5afd103344eeb50262ea369a376c358
Edi Prayitno <its.edi@gmail.com>
2025-05-27 12:27:21 +0700

    fix

--- src/position.cpp
+++ src/position.cpp
@@ -43,7 +43,7 @@ namespace equipment_tracker
                        std::sin(dlon / 2) * std::sin(dlon / 2);
         double c = 2 * std::atan2(std::sqrt(a), std::sqrt(1 - a));
 
-       // Distance in meters
+        // Distance in meters
         return EARTH_RADIUS_METERS * c;
     }
 
